e0ffe48d80749d240d5905bd96068574890317ee
Ryan McCrory <ryanmccrory2@Ryans-MacBook-Pro-2.local>
2018-06-14 11:11:24 -0600

    config issues

---
+++ app.js
@@ -0,0 +1,43 @@
+//Your code goes here
+const canvas = document.querySelector('.canvas')
+let currentColor
+
+function createCanvas() {
+    for (let i = 0; i < 504; i++) {
+        const cell = document.createElement('div')
+        cell.classList.add('cell')
+
+        cell.addEventListener('click', (event => {
+            event.target.style.backgroundColor = currentColor
+        }))
+
+        canvas.appendChild(cell)
+    }
+}
+
+const colorPalette = document.querySelector('.color-palette')
+
+const colors = ['firebrick', 'dodgerblue', 'goldenrod', 'wheat', 'chartreuse', 'salmon', 'white','red','green','orange','purple','black']
+
+function createColorPalette (){
+    colors.forEach(color => {
+        const brush = document.createElement('div')
+        brush.classList.add('brush')
+        brush.style.backgroundColor = color
+        brush.style.border = "thin solid black"; 
+        brush.dataset.color = color
+
+        brush.addEventListener('click', (event => {
+            currentColor = event.target.dataset.color
+            console.log(currentColor)
+            for(let i = 0; i < document.querySelectorAll('.brush').length; i++){
+                document.querySelectorAll('.brush')[i].style.border = "thin solid black";
+            }
+            event.target.style.border = "thick solid black"; 
+        }))
+
+        colorPalette.appendChild(brush)
+    })
+}
+createCanvas();
+createColorPalette();
